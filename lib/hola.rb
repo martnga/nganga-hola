faaa52e4bc2d031eefa62fb1d9c6812ca1f6b3bf
mtnjoroge <mtnjoroge@gmail.com>
2015-08-18 17:10:22 +0300

    adde the documentation

--- lib/hola.rb
+++ lib/hola.rb
@@ -1,8 +1,17 @@
+require 'hola/translator'
 class Hola
+
+  # Say hi to the world!
+  #
+  # Example:
+  #   >> Hola.hi("spanish")
+  #   => hola mundo
+  #
+  # Arguments:
+  #   language: (String)
+
   def self.hi(language = "english")
     translator = Translator.new(language)
     translator.hi
   end
 end
-
-require 'hola/translator'
